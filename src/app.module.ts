43e75d4d75f9a8894188afc575f002150234f997
rizalord <ahmadkhamdani9@gmail.com>
2026-06-11 14:37:58 +0700

    feat: setup prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [AuthModule, CategoriesModule, ProductsModule],
+  imports: [PrismaModule, AuthModule, CategoriesModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
